a131fe0f7b874bd46003c9cf48e396f04d1b3427
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-06-28 19:40:15 -0400

    Memory-Game-part1

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import MemoryCard from './MemoryCard.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Memory Game</h1>
+          <h2 className="App-subtitle">Match Cards To Win</h2>
+        </header>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
