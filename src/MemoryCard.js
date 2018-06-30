ac61f22100316792d8976d2574db087c72c545a9
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-06-30 02:07:36 -0400

    Memory-Game-part2

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
-  render() {
-    return (
-      <div className="MemoryCard">
-        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
-      </div>
-    );
-  }
+    constructor(){
+        super()
+        this.state = { isFlipped: false };
+    }
+    
+    clickHandler = () => {
+        this.setState(
+        { isFlipped: !this.state.isFlipped }
+        );
+    }
+
+    render() {
+        let memoryCardInnerClass = "MemoryCardInner";
+        if (this.state.isFlipped === true) {
+            memoryCardInnerClass= memoryCardInnerClass + ' flipped';
+        }
+        return (
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
+                    </div>
+                    <div className="MemoryCardFront">
+                    ∆
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MemoryCard;
